ba72d5f4029029a01e8570471f6c0d36fd7cdef8
Lawrence Tran <lawrence.c.tran@outlook.com>
2017-08-30 18:57:36 -0700

    Add button icons and logo

--- RandomEmojiKeyboard/KeyboardViewController.m
+++ RandomEmojiKeyboard/KeyboardViewController.m
@@ -14,6 +14,7 @@
 @property (nonatomic, strong) UIButton *nextKeyboardButton;
 @property (nonatomic, strong) UIButton *emojiButton;
 @property (nonatomic, strong) UIButton *deleteButton;
+@property (nonatomic, strong) UILabel *logoLabel;
 @end
 
 @implementation KeyboardViewController
@@ -26,9 +27,11 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  [self setupLogo];
   [self setupEmojiButton];
   [self setupNextKeyboardButton];
   [self setupDeleteButton];
+  self.view.backgroundColor = [UIColor colorWithRed:240.0f/255.0f green:248.0f/255.0f blue:255.0f/255.0f alpha:1.0f];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -54,10 +57,26 @@
 
 #pragma mark - Setup
 
+- (void)setupLogo {
+  
+  self.logoLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+  [self.logoLabel setText:@"Random Emoji Keyboard"];
+  [self.logoLabel setFont:[UIFont boldSystemFontOfSize:10.0]];
+  [self.logoLabel setAlpha:0.2f];
+  [self.logoLabel sizeToFit];
+  self.logoLabel.translatesAutoresizingMaskIntoConstraints = NO;
+  
+  [self.view addSubview:self.logoLabel];
+  
+  [self.logoLabel.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-18.0].active = YES;
+  [self.logoLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+}
+
 - (void)setupEmojiButton {
   
   self.emojiButton = [UIButton buttonWithType:UIButtonTypeSystem];
-  [self.emojiButton setTitle:@"Emoji" forState:UIControlStateNormal];
+  [self.emojiButton setTitle:@"\U0001F602" forState:UIControlStateNormal];
+  [self.emojiButton.titleLabel setFont:[UIFont systemFontOfSize:102.0]];
   [self.emojiButton sizeToFit];
   self.emojiButton.translatesAutoresizingMaskIntoConstraints = NO;
   [self.emojiButton addTarget:self action:@selector(emojiButtonPressed) forControlEvents:UIControlEventTouchUpInside];
@@ -65,35 +84,37 @@
   [self.view addSubview:self.emojiButton];
   
   [self.emojiButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
-  [self.emojiButton.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor].active = YES;
+  [self.emojiButton.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor constant:-23.0].active = YES;
 }
 
 - (void)setupDeleteButton {
   
   self.deleteButton = [UIButton buttonWithType:UIButtonTypeSystem];
-  [self.deleteButton setTitle:@"Delete" forState:UIControlStateNormal];
+  [self.deleteButton setTitle:@"\U0001F697" forState:UIControlStateNormal];
+  [self.deleteButton.titleLabel setFont:[UIFont systemFontOfSize:30.0]];
   [self.deleteButton sizeToFit];
   self.deleteButton.translatesAutoresizingMaskIntoConstraints = NO;
   [self.deleteButton addTarget:self action:@selector(deleteButtonPressed) forControlEvents:UIControlEventTouchUpInside];
   
   [self.view addSubview:self.deleteButton];
   
-  [self.deleteButton.rightAnchor constraintEqualToAnchor:self.view.rightAnchor].active = YES;
-  [self.deleteButton.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
+  [self.deleteButton.rightAnchor constraintEqualToAnchor:self.view.rightAnchor constant: -40.0].active = YES;
+  [self.deleteButton.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant: -5.0].active = YES;
 }
 
 - (void)setupNextKeyboardButton {
   
   self.nextKeyboardButton = [UIButton buttonWithType:UIButtonTypeSystem];
-  [self.nextKeyboardButton setTitle:NSLocalizedString(@"Next Keyboard", @"Title for 'Next Keyboard' button") forState:UIControlStateNormal];
+  [self.nextKeyboardButton setTitle:@"\U0001F310" forState:UIControlStateNormal];
+  [self.nextKeyboardButton.titleLabel setFont:[UIFont systemFontOfSize:25.0]];
   [self.nextKeyboardButton sizeToFit];
   self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = NO;
   [self.nextKeyboardButton addTarget:self action:@selector(handleInputModeListFromView:withEvent:) forControlEvents:UIControlEventAllTouchEvents];
   
   [self.view addSubview:self.nextKeyboardButton];
   
-  [self.nextKeyboardButton.leftAnchor constraintEqualToAnchor:self.view.leftAnchor].active = YES;
-  [self.nextKeyboardButton.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
+  [self.nextKeyboardButton.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant: 40.0].active = YES;
+  [self.nextKeyboardButton.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant: -5.0].active = YES;
 }
 
 #pragma mark - Private Methods
